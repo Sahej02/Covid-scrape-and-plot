048c9dba429ffbd654a5eea77970f3a19a5ae297
sahej02 <sahejveer87@gmail.com>
2020-03-28 15:34:19 +0530

    Modified columns in df.py

--- df.py
+++ df.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-dict1 = {"Time": [], "IN": [], "FN": [], "Dead":[], '"Total"': [] }
+dict1 = {"Time": [], "IN": [], "FN": [], "Cured/Di/M": [] ,"Dead":[], '"Total"': [] }
 data = pd.DataFrame(dict1, index = None)
+print(data.head)
 #data.to_csv("enter_name_here.csv", index = False)
