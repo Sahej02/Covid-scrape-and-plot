2477a251ada3fb268a4d83f1435dbe45d696dd52
sahej02 <sahejveer87@gmail.com>
2020-03-28 15:27:48 +0530

    added record_grapher.py to plot the data

---
+++ record_grapher.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.dates as mpdt
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import re
+from datetime import datetime
+from pandas.plotting import register_matplotlib_converters
+
+
+def dt(dates):
+	dtimes = []
+	for x in dates:
+		dtime = re.sub(r'\sat\s', " ", x)
+		date_obj = datetime.strptime(dtime, "%d.%m.%Y %I:%M %p")
+		dtimes.append(date_obj)
+	return dtimes
+
+register_matplotlib_converters()
+df = pd.read_csv("covid_data.csv", header = 0, index_col = False)
+dates = df['Time']
+dtimes = dt(dates)
+
+final_dates = mpdt.date2num(dtimes)
+plt.plot_date(final_dates, df['"Total"'], '-')
+plt.show()
