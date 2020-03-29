0ae4071f70eeef96689bbbdf23007d671508f85b
sahej02 <sahejveer87@gmail.com>
2020-03-29 21:41:36 +0530

    changed acc to website changes

--- df.py
+++ df.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-dict1 = {"Time": [], "IN": [], "FN": [], "Cured/Di/M": [] ,"Dead":[], '"Total"': [] }
+#dict1 = {"Time": [], "IN": [], "FN": [], "Cured/Di/M": [] ,"Dead":[], '"Total"': [] }
+dict1 = {"Time": [], "Confirmed": [], "Cured/Di/M": [] ,"Dead":[], '"Total"': [] }
 data = pd.DataFrame(dict1, index = None)
 print(data.head)
 #data.to_csv("enter_name_here.csv", index = False)
